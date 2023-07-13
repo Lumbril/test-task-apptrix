47da35625219c9babfb1fc74b5530bf881587c74
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-07-13 21:39:02 +0300

    fix

--- src/api/models/user_model.py
+++ src/api/models/user_model.py
@@ -5,6 +5,33 @@ from django.db import models
 from api.packs import watermark_photo
 
 
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+
+        extra_fields.setdefault('is_active', False)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, blank=False, null=False, verbose_name='Почта')
@@ -17,6 +44,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def get_avatar(self):
         return str(self.avatar) if self.avatar else None
