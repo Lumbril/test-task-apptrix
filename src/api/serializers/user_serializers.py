9b6ef2c3e43d7e6180a33630e31d6a5ce3969387
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-07-13 23:23:44 +0300

    Update user_serializers.py

--- src/api/serializers/user_serializers.py
+++ src/api/serializers/user_serializers.py
@@ -9,7 +9,7 @@ class UserResponseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserModel
-        fields = ['email', 'first_name', 'last_name', 'avatar', 'gender',
+        fields = ['id', 'email', 'first_name', 'last_name', 'avatar', 'gender',
                   'latitude', 'longitude']
 
 
